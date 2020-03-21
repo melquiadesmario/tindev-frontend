657eabe01f0cc6e509aa4b2534c792edb153c239
melquiadesmario <melquiadesm@gmail.com>
2020-03-20 22:45:17 -0300

    Implementation of routes

---
+++ src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+import logo from '../assets/logo.svg'
+
+export default function Login(){
+    return(
+        <div className='login-container'>
+            <form>
+                <img src={ logo } alt='TinDev' />
+                <input
+                    placeholder='Enter your Github user'
+                />
+                <button type='submit'>Send</button>
+            </form>
+        </div>
+    )
+}
